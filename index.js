1eea974303f274eb7671c3ca7fce8aa12b8ec110
Andrey Sitnik <andrey@sitnik.ru>
2014-12-14 18:11:54 +0300

    Use browserslist() instead of browserslist.get()

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var caniuse = require('caniuse-db/data').agents;
-
 var uniq = function (array) {
     var filtered = [];
     for ( var i = 0; i < array.length; i++ ) {
@@ -8,181 +6,184 @@ var uniq = function (array) {
     return filtered;
 };
 
-var browserslist = {
+// Return array of browsers by selection queries:
+//
+//   browserslist('IE >= 10, IE 8') //=> ['ie 11', 'ie 10', 'ie 8']
+browserslist = function (selections) {
+    var result = [];
 
-    // Converted Can I Use data
-    data: { },
+    if ( typeof(selections) == 'undefined' ) {
+        selections = browserslist.defaults;
+    } else if ( typeof(selections) == 'string' ) {
+        selections = selections.split(/,\s*/);
+    }
 
-    // Usage statistics from Can I Use
-    usage: {
-        global: { }
-    },
+    var query, match, array, used;
+    selections.forEach(function (selection) {
+        if ( selection.trim() === '' ) return;
+        used = false;
 
-    // Default browsers query
-    defaults: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'],
-
-    // What browsers will be used in `last n version` query
-    major: ['firefox', 'chrome',  'safari', 'ios_saf',
-            'opera',   'android', 'ie',     'ie_mob'],
-
-    // Browser names aliases
-    aliases: {
-        fx:             'firefox',
-        ff:             'firefox',
-        ios:            'ios_saf',
-        explorer:       'ie',
-        blackberry:     'bb',
-        explorermobile: 'ie_mob',
-        operamini:      'op_mini',
-        operamobile:    'op_mob',
-        chromeandroid:  'and_chr',
-        firefoxandroid: 'and_ff'
-    },
-
-    // Return array of browsers by selection queries:
-    //
-    //   browserslist.get('IE >= 10, IE 8') //=> ['ie 11', 'ie 10', 'ie 8']
-    get: function (selections) {
-        var result = [];
-
-        if ( typeof(selections) == 'undefined' ) {
-            selections = browserslist.defaults;
-        } else if ( typeof(selections) == 'string' ) {
-            selections = selections.split(/,\s*/);
-        }
-
-        var query, match, array, used;
-        selections.forEach(function (selection) {
-            if ( selection.trim() === '' ) return;
-            used = false;
-
-            for ( var i in browserslist.queries ) {
-                query = browserslist.queries[i];
-                match = selection.match(query.regexp);
-                if ( match ) {
-                    array  = query.select.apply(browserslist, match.slice(1));
-                    result = result.concat(array);
-                    used   = true;
-                    break;
-                }
-            }
-
-            if ( !used ) {
-                throw 'Unknown browser query `' + selection + '`';
-            }
-        });
-
-        return uniq(result);
-    },
-
-    // Get browser data by alias or case insensitive name
-    byName: function (name) {
-        name = name.toLowerCase();
-        name = browserslist.aliases[name] || name;
-
-        var data = browserslist.data[name];
-        if ( !data ) throw 'Unknown browser ' + name;
-        return data;
-    },
-
-    queries: {
-
-        lastVersions: {
-            regexp: /^last (\d+) versions?$/i,
-            select: function (versions) {
-                var selected = [];
-                browserslist.major.forEach(function (name) {
-                    var data  = browserslist.byName(name);
-                    var array = data.released.slice(-versions);
-
-                    array = array.map(function (v) {
-                        return data.name + ' ' + v;
-                    });
-                    selected = selected.concat(array);
-                });
-                return selected;
-            }
-        },
-
-        lastByBrowser: {
-            regexp: /^last (\d+) (\w+) versions?$/i,
-            select: function (versions, name) {
-                var data = browserslist.byName(name);
-                return data.released.slice(-versions).map(function (v) {
-                    return data.name + ' ' + v;
-                });
-            }
-        },
-
-        statistics: {
-            regexp: /^> (\d+(\.\d+)?)%$/,
-            select: function (popularity) {
-                popularity = parseFloat(popularity);
-                var result = [];
-
-                for ( var version in browserslist.usage.global ) {
-                    if ( browserslist.usage.global[version] > popularity ) {
-                        result.push(version);
-                    }
-                }
-
-                return result;
-            }
-        },
-
-        versions: {
-            regexp: /^(\w+) (>=?|<=?)\s*([\d\.]+)/,
-            select: function (name, sign, version) {
-                var data = browserslist.byName(name);
-                version  = parseFloat(version);
-
-                var filter;
-                if ( sign == '>' ) {
-                    filter = function (v) {
-                        return parseFloat(v) > version;
-                    };
-                } else if ( sign == '>=' ) {
-                    filter = function (v) {
-                        return parseFloat(v) >= version;
-                    };
-                } else if ( sign == '<' ) {
-                    filter = function (v) {
-                        return parseFloat(v) < version;
-                    };
-                } else if ( sign == '<=' ) {
-                    filter = function (v) {
-                        return parseFloat(v) <= version;
-                    };
-                }
-
-                return data.released.filter(filter).map(function (v) {
-                    return data.name + ' ' + v;
-                });
-            }
-        },
-
-        esr: {
-            regexp: /^(firefox|ff|fx) esr$/i,
-            select: function (versions) {
-                return ['firefox 31'];
-            }
-        },
-
-        direct: {
-            regexp: /^(\w+) ([\d\.]+)$/,
-            select: function (name, version) {
-                var data = browserslist.byName(name);
-                if ( data.versions.indexOf(version) == -1 ) {
-                    throw 'Unknown version ' + version + ' of ' + name;
-                }
-
-                return [data.name + ' ' + version];
+        for ( var i in browserslist.queries ) {
+            query = browserslist.queries[i];
+            match = selection.match(query.regexp);
+            if ( match ) {
+                array  = query.select.apply(browserslist, match.slice(1));
+                result = result.concat(array);
+                used   = true;
+                break;
             }
         }
 
+        if ( !used ) {
+            throw 'Unknown browser query `' + selection + '`';
+        }
+    });
+
+    return uniq(result);
+};
+
+// Will be filled by Can I Use data below
+browserslist.data  = { };
+browserslist.usage = {
+    global: { }
+};
+
+// Default browsers query
+browserslist.defaults = [
+    '> 1%',
+    'last 2 versions',
+    'Firefox ESR',
+    'Opera 12.1'
+];
+
+// What browsers will be used in `last n version` query
+browserslist.major = ['firefox', 'chrome',  'safari', 'ios_saf',
+                      'opera',   'android', 'ie',     'ie_mob'];
+
+// Browser names aliases
+browserslist.aliases = {
+    fx:             'firefox',
+    ff:             'firefox',
+    ios:            'ios_saf',
+    explorer:       'ie',
+    blackberry:     'bb',
+    explorermobile: 'ie_mob',
+    operamini:      'op_mini',
+    operamobile:    'op_mob',
+    chromeandroid:  'and_chr',
+    firefoxandroid: 'and_ff'
+};
+
+// Get browser data by alias or case insensitive name
+browserslist.byName = function (name) {
+    name = name.toLowerCase();
+    name = browserslist.aliases[name] || name;
+
+    var data = browserslist.data[name];
+    if ( !data ) throw 'Unknown browser ' + name;
+    return data;
+};
+
+browserslist.queries = {
+
+    lastVersions: {
+        regexp: /^last (\d+) versions?$/i,
+        select: function (versions) {
+            var selected = [];
+            browserslist.major.forEach(function (name) {
+                var data  = browserslist.byName(name);
+                var array = data.released.slice(-versions);
+
+                array = array.map(function (v) {
+                    return data.name + ' ' + v;
+                });
+                selected = selected.concat(array);
+            });
+            return selected;
+        }
+    },
+
+    lastByBrowser: {
+        regexp: /^last (\d+) (\w+) versions?$/i,
+        select: function (versions, name) {
+            var data = browserslist.byName(name);
+            return data.released.slice(-versions).map(function (v) {
+                return data.name + ' ' + v;
+            });
+        }
+    },
+
+    statistics: {
+        regexp: /^> (\d+(\.\d+)?)%$/,
+        select: function (popularity) {
+            popularity = parseFloat(popularity);
+            var result = [];
+
+            for ( var version in browserslist.usage.global ) {
+                if ( browserslist.usage.global[version] > popularity ) {
+                    result.push(version);
+                }
+            }
+
+            return result;
+        }
+    },
+
+    versions: {
+        regexp: /^(\w+) (>=?|<=?)\s*([\d\.]+)/,
+        select: function (name, sign, version) {
+            var data = browserslist.byName(name);
+            version  = parseFloat(version);
+
+            var filter;
+            if ( sign == '>' ) {
+                filter = function (v) {
+                    return parseFloat(v) > version;
+                };
+            } else if ( sign == '>=' ) {
+                filter = function (v) {
+                    return parseFloat(v) >= version;
+                };
+            } else if ( sign == '<' ) {
+                filter = function (v) {
+                    return parseFloat(v) < version;
+                };
+            } else if ( sign == '<=' ) {
+                filter = function (v) {
+                    return parseFloat(v) <= version;
+                };
+            }
+
+            return data.released.filter(filter).map(function (v) {
+                return data.name + ' ' + v;
+            });
+        }
+    },
+
+    esr: {
+        regexp: /^(firefox|ff|fx) esr$/i,
+        select: function (versions) {
+            return ['firefox 31'];
+        }
+    },
+
+    direct: {
+        regexp: /^(\w+) ([\d\.]+)$/,
+        select: function (name, version) {
+            var data = browserslist.byName(name);
+            if ( data.versions.indexOf(version) == -1 ) {
+                throw 'Unknown version ' + version + ' of ' + name;
+            }
+
+            return [data.name + ' ' + version];
+        }
     }
 
 };
+
+// Get and convert Can I Use data
+
+var caniuse = require('caniuse-db/data').agents;
 
 var normalizeVersion = function (version) {
     return version.split('-')[0];
--- test/browserslist.js
+++ test/browserslist.js
@@ -4,237 +4,229 @@ var expect       = require('chai').expect;
 var originData  = browserslist.data;
 var originUsage = browserslist.usage;
 
-describe('browserslist', function () {
+describe('browserslist()', function () {
 
     afterEach(function () {
         browserslist.data  = originData;
         browserslist.usage = originUsage;
     });
 
-    describe('.get()', function () {
+    it('accepts array', function () {
+        expect(browserslist(['ie 11', 'ie 10'])).to.eql(['ie 11', 'ie 10']);
+    });
 
-        it('accepts array', function () {
-            expect(browserslist.get(['ie 11', 'ie 10']))
-                .to.eql(['ie 11', 'ie 10']);
+    it('accepts string', function () {
+        expect(browserslist('ie 11, ie 10')).to.eql(['ie 11', 'ie 10']);
+    });
+
+    it('returns unique array', function () {
+        expect(browserslist('ie 10, ie 10')).to.eql(['ie 10']);
+    });
+
+    it('returns empty result', function () {
+        expect(browserslist([])).to.eql([]);
+        expect(browserslist('')).to.eql([]);
+    });
+
+    it('has default selection', function () {
+        expect(browserslist.defaults.length).to.be.at.least(1);
+    });
+
+    it('use default selection on empty request', function () {
+        expect(browserslist())
+            .to.eql(browserslist(browserslist.defaults));
+    });
+
+    it('raises on unknow query', function () {
+        expect(function () {
+            browserslist('good');
+        }).to.throw('Unknown browser query `good`');
+    });
+
+    describe('ESR query', function () {
+
+        it('selects Firefox ESR', function () {
+            expect(browserslist('Firefox ESR')).to.eql(['firefox 31']);
         });
 
-        it('accepts string', function () {
-            expect(browserslist.get('ie 11, ie 10'))
-                .to.eql(['ie 11', 'ie 10']);
+        it('uses case insensitive aliases', function () {
+            var result = browserslist('Firefox ESR');
+            expect(browserslist('firefox esr')).to.eql(result);
+            expect(browserslist('ff esr')).to.eql(result);
+            expect(browserslist('fx esr')).to.eql(result);
         });
 
-        it('returns unique array', function () {
-            expect(browserslist.get('ie 10, ie 10')).to.eql(['ie 10']);
+    });
+
+    describe('direct query', function () {
+
+        it('selects browser by name', function () {
+            expect(browserslist('ie 10')).to.eql(['ie 10']);
         });
 
-        it('returns empty result', function () {
-            expect(browserslist.get([])).to.eql([]);
-            expect(browserslist.get('')).to.eql([]);
+        it('uses case insensitive aliases', function () {
+            var result = browserslist('ie 10');
+            expect(browserslist('Explorer 10')).to.eql(result);
+            expect(browserslist('IE 10')).to.eql(result);
         });
 
-        it('has default selection', function () {
-            expect(browserslist.defaults.length).to.be.at.least(1);
-        });
-
-        it('use default selection on empty request', function () {
-            expect(browserslist.get())
-                .to.eql(browserslist.get(browserslist.defaults));
-        });
-
-        it('raises on unknow query', function () {
+        it('raises on unknown name', function () {
             expect(function () {
-                browserslist.get('good');
-            }).to.throw('Unknown browser query `good`');
+                browserslist('unknow 10');
+            }).to.throw('Unknown browser unknow');
         });
 
-        describe('ESR query', function () {
-
-            it('selects Firefox ESR', function () {
-                expect(browserslist.get('Firefox ESR')).to.eql(['firefox 31']);
-            });
-
-            it('uses case insensitive aliases', function () {
-                var result = browserslist.get('Firefox ESR');
-                expect(browserslist.get('firefox esr')).to.eql(result);
-                expect(browserslist.get('ff esr')).to.eql(result);
-                expect(browserslist.get('fx esr')).to.eql(result);
-            });
-
+        it('raises on unknown version', function () {
+            expect(function () {
+                browserslist('IE 1');
+            }).to.throw('Unknown version 1 of IE');
         });
 
-        describe('direct query', function () {
+    });
 
-            it('selects browser by name', function () {
-                expect(browserslist.get('ie 10')).to.eql(['ie 10']);
-            });
+    describe('versions query', function () {
 
-            it('uses case insensitive aliases', function () {
-                var result = browserslist.get('ie 10');
-                expect(browserslist.get('Explorer 10')).to.eql(result);
-                expect(browserslist.get('IE 10')).to.eql(result);
-            });
-
-            it('raises on unknown name', function () {
-                expect(function () {
-                    browserslist.get('unknow 10');
-                }).to.throw('Unknown browser unknow');
-            });
-
-            it('raises on unknown version', function () {
-                expect(function () {
-                    browserslist.get('IE 1');
-                }).to.throw('Unknown version 1 of IE');
-            });
-
+        beforeEach(function () {
+            browserslist.data = {
+                ie: {
+                    name:      'ie',
+                    released: ['8', '9', '10', '11'],
+                    versions: ['8', '9', '10', '11', '12']
+                }
+            };
         });
 
-        describe('versions query', function () {
-
-            beforeEach(function () {
-                browserslist.data = {
-                    ie: {
-                        name:      'ie',
-                        released: ['8', '9', '10', '11'],
-                        versions: ['8', '9', '10', '11', '12']
-                    }
-                };
-            });
-
-            it('selects browser by more sign', function () {
-                expect(browserslist.get('ie > 9')).to.eql(['ie 10', 'ie 11']);
-            });
-
-            it('selects browser by more or equal sign', function () {
-                expect(browserslist.get('ie >= 10')).to.eql(['ie 10', 'ie 11']);
-            });
-
-            it('selects browser by less sign', function () {
-                expect(browserslist.get('ie < 10')).to.eql(['ie 8', 'ie 9']);
-            });
-
-            it('selects browser by less or equal sign', function () {
-                expect(browserslist.get('ie <= 9')).to.eql(['ie 8', 'ie 9']);
-            });
-
-            it('uses case insensitive aliases', function () {
-                expect(browserslist.get('Explorer > 10')).to.eql(['ie 11']);
-            });
-
-            it('raises on unknown browser', function () {
-                expect(function () {
-                    browserslist.get('unknow > 10');
-                }).to.throw('Unknown browser unknow');
-            });
-
+        it('selects browser by more sign', function () {
+            expect(browserslist('ie > 9')).to.eql(['ie 10', 'ie 11']);
         });
 
-        describe('last n version query', function () {
-
-            beforeEach(function () {
-                browserslist.data = {
-                    ie: {
-                        name:      'ie',
-                        released: ['9', '10', '11'],
-                        versions: ['9', '10', '11', '12']
-                    },
-                    chrome: {
-                        name:      'chrome',
-                        released: ['37', '38', '39'],
-                        versions: ['37', '38', '39', '40']
-                    },
-                    blackberry: {
-                        name:      'blackberry',
-                        released: ['8'],
-                        versions: []
-                    },
-                    firefox: {
-                        released: []
-                    },
-                    safari: {
-                        released: []
-                    },
-                    ios_saf: {
-                        released: []
-                    },
-                    opera: {
-                        released: []
-                    },
-                    android: {
-                        released: []
-                    },
-                    ie_mob: {
-                        released: []
-                    }
-                };
-            });
-
-            it('selects versions of each major browser', function () {
-                expect(browserslist.get('last 2 versions'))
-                    .to.eql(['chrome 38', 'chrome 39', 'ie 10', 'ie 11']);
-            });
-
-            it('supports pluralization', function () {
-                expect(browserslist.get('last 1 version'))
-                    .to.eql(['chrome 39', 'ie 11']);
-            });
-
-            it('is case insensitive', function () {
-                expect(browserslist.get('Last 01 Version'))
-                    .to.eql(['chrome 39', 'ie 11']);
-            });
-
+        it('selects browser by more or equal sign', function () {
+            expect(browserslist('ie >= 10')).to.eql(['ie 10', 'ie 11']);
         });
 
-        describe('last n version of browser query', function () {
-
-            beforeEach(function () {
-                browserslist.data = {
-                    ie: {
-                        name:      'ie',
-                        released: ['9', '10', '11'],
-                        versions: ['9', '10', '11', '12']
-                    }
-                };
-            });
-
-            it('selects versions of browser', function () {
-                expect(browserslist.get('last 2 ie versions'))
-                    .to.eql(['ie 10', 'ie 11']);
-            });
-
-            it('supports pluralization', function () {
-                expect(browserslist.get('last 1 ie version'))
-                    .to.eql(['ie 11']);
-            });
-
-            it('has case insensitive aliases', function () {
-                expect(browserslist.get('Last 01 Explorer Version'))
-                    .to.eql(['ie 11']);
-            });
-
+        it('selects browser by less sign', function () {
+            expect(browserslist('ie < 10')).to.eql(['ie 8', 'ie 9']);
         });
 
-        describe('global statistics query', function () {
-
-            beforeEach(function () {
-                browserslist.usage = {
-                    global: {
-                        'ie 9':  5,
-                        'ie 10': 10.1,
-                        'ie 11': 75
-                    }
-                };
-            });
-
-            it('selects browsers by popularity', function () {
-                expect(browserslist.get('> 10%')).to.eql(['ie 10', 'ie 11']);
-            });
-
-            it('works with float', function () {
-                expect(browserslist.get('> 10.2%')).to.eql(['ie 11']);
-            });
+        it('selects browser by less or equal sign', function () {
+            expect(browserslist('ie <= 9')).to.eql(['ie 8', 'ie 9']);
         });
 
+        it('uses case insensitive aliases', function () {
+            expect(browserslist('Explorer > 10')).to.eql(['ie 11']);
+        });
+
+        it('raises on unknown browser', function () {
+            expect(function () {
+                browserslist('unknow > 10');
+            }).to.throw('Unknown browser unknow');
+        });
+
+    });
+
+    describe('last n version query', function () {
+
+        beforeEach(function () {
+            browserslist.data = {
+                ie: {
+                    name:      'ie',
+                    released: ['9', '10', '11'],
+                    versions: ['9', '10', '11', '12']
+                },
+                chrome: {
+                    name:      'chrome',
+                    released: ['37', '38', '39'],
+                    versions: ['37', '38', '39', '40']
+                },
+                blackberry: {
+                    name:      'blackberry',
+                    released: ['8'],
+                    versions: []
+                },
+                firefox: {
+                    released: []
+                },
+                safari: {
+                    released: []
+                },
+                ios_saf: {
+                    released: []
+                },
+                opera: {
+                    released: []
+                },
+                android: {
+                    released: []
+                },
+                ie_mob: {
+                    released: []
+                }
+            };
+        });
+
+        it('selects versions of each major browser', function () {
+            expect(browserslist('last 2 versions'))
+                .to.eql(['chrome 38', 'chrome 39', 'ie 10', 'ie 11']);
+        });
+
+        it('supports pluralization', function () {
+            expect(browserslist('last 1 version'))
+                .to.eql(['chrome 39', 'ie 11']);
+        });
+
+        it('is case insensitive', function () {
+            expect(browserslist('Last 01 Version'))
+                .to.eql(['chrome 39', 'ie 11']);
+        });
+
+    });
+
+    describe('last n version of browser query', function () {
+
+        beforeEach(function () {
+            browserslist.data = {
+                ie: {
+                    name:      'ie',
+                    released: ['9', '10', '11'],
+                    versions: ['9', '10', '11', '12']
+                }
+            };
+        });
+
+        it('selects versions of browser', function () {
+            expect(browserslist('last 2 ie versions'))
+                .to.eql(['ie 10', 'ie 11']);
+        });
+
+        it('supports pluralization', function () {
+            expect(browserslist('last 1 ie version')).to.eql(['ie 11']);
+        });
+
+        it('has case insensitive aliases', function () {
+            expect(browserslist('Last 01 Explorer Version')).to.eql(['ie 11']);
+        });
+
+    });
+
+    describe('global statistics query', function () {
+
+        beforeEach(function () {
+            browserslist.usage = {
+                global: {
+                    'ie 9':  5,
+                    'ie 10': 10.1,
+                    'ie 11': 75
+                }
+            };
+        });
+
+        it('selects browsers by popularity', function () {
+            expect(browserslist('> 10%')).to.eql(['ie 10', 'ie 11']);
+        });
+
+        it('works with float', function () {
+            expect(browserslist('> 10.2%')).to.eql(['ie 11']);
+        });
     });
 
 });
