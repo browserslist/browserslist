6ed99031c651d7c8d40efbdf8464f1da62dc4f43
Andrey Sitnik <andrey@sitnik.ru>
2016-03-16 12:19:11 +0600

    Add --coverage option to CLI

--- cli.js
+++ cli.js
@@ -8,27 +8,53 @@ function isArg(arg) {
     return args.indexOf(arg) >= 0;
 }
 
-if ( args.length === 0 || isArg('--help') || isArg('-h') ) {
-    process.stdout.write([
-        pkg.description,
-        '',
-        'Usage: ' + pkg.name + ' "QUERIES"'
-    ].join('\n') + '\n');
+function error(msg) {
+    process.stderr.write(pkg.name + ': ' + msg + '\n');
+    process.exit(1);
+}
 
-} else if ( isArg('--version') ) {
-    process.stdout.write(pkg.name + ' ' + pkg.version + '\n');
-
-} else {
+function query(queries) {
     try {
-        browserslist(args[0]).forEach(function (browser) {
-            process.stdout.write(browser + '\n');
-        });
+        return browserslist(queries);
     } catch (e) {
         if ( e.name === 'BrowserslistError' ) {
-            process.stderr.write(pkg.name + ': ' + e.message + '\n');
-            process.exit(1);
+            error(e.message);
         } else {
             throw e;
         }
     }
+}
+
+if ( args.length === 0 || isArg('--help') || isArg('-h') ) {
+    process.stdout.write([
+        pkg.description,
+        '',
+        'Usage:',
+        '  ' + pkg.name + ' "QUERIES"',
+        '  ' + pkg.name + ' --coverage "QUERIES"'
+    ].join('\n') + '\n');
+
+} else if ( isArg('--version') || isArg('-v') ) {
+    process.stdout.write(pkg.name + ' ' + pkg.version + '\n');
+
+} else if ( isArg('--version') || isArg('-v') ) {
+    process.stdout.write(pkg.name + ' ' + pkg.version + '\n');
+
+} else if ( isArg('--coverage') || isArg('-c') ) {
+    var browsers = args.find(function (i) {
+        return i[0] !== '-';
+    });
+    if ( !browsers ) error('Define a browsers query to get coverage');
+    var result = browserslist.coverage(query(browsers));
+    var round  = Math.round(result * 100) / 100.0;
+    process.stdout.write(
+        'These browsers account for ' + round + '% of all users globally\n');
+
+} else if ( args.length === 1 && args[0][0] !== '-' ) {
+    query(args[0]).forEach(function (browser) {
+        process.stdout.write(browser + '\n');
+    });
+
+} else {
+    error('Unknown arguments. Use --help to pick right one.')
 }
