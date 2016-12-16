4008590e2bcb342f5c44b406bae3d608b90c4205
openlibser <tmpmail@email.ml>
2016-12-16 17:02:34 +1000

    Added different environments

--- test/config.test.js
+++ test/config.test.js
@@ -5,6 +5,7 @@ var path = require('path');
 var css = path.join(__dirname, 'fixtures', 'dir', 'test.css');
 var withBoth = path.join(__dirname, 'fixtures', 'withBoth', 'test.css');
 var withPackage = path.join(__dirname, 'fixtures', 'withPackage', 'test.css');
+var withSections = path.join(__dirname, 'fixtures', 'withSections', 'test.css');
 
 it('parses queries', () => {
     expect(browserslist.parseConfig('ie 10\n> 1%')).toEqual(['ie 10', '> 1%']);
@@ -36,4 +37,19 @@ it('reads from dir wich contains both browserslist and package.json', () => {
     expect( () => {
         browserslist.readConfig(withBoth);
     }).toThrowError(/contains both/);
+});
+
+it('reads config with sections wihout env', () => {
+    // by default env is development
+    expect(browserslist.readConfig(withSections))
+        .toEqual(['ie 10', 'ie 11', 'chrome 53']);
+});
+
+it('reads config with specific env', () => {
+    expect(browserslist.readConfig(withSections, 'custom'))
+        .toEqual(['chrome 51', 'ff 42']);
+    expect(browserslist.readConfig(withSections, 'production'))
+        .toEqual(['ie 11', 'ie 10']);
+    expect(browserslist.readConfig(withSections, 'development'))
+        .toEqual(['ie 10', 'ie 11', 'chrome 53']);
 });
