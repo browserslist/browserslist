browserslist/browserslist

commit bc86e87e0e958eaa05fb2c2017625e69ea243699

Reimplement `eachParent`'s traversal (#117)
Previously `eachParent` sliced the path all the way up to the empty string.

Practically this meant that calls to `eachParent` ended up _also_ taking
into consideration a `package.json` in the current working directory.

This implementation is based on the traversal code in `babel`.

index.js
@@ -55,12 +55,10 @@ function eachParent(file, callback) {
 
     if ( file === false ) return undefined;
     if ( typeof file === 'undefined' ) file = '.';
-
-    var dirs = path.resolve(file).split(path.sep);
-    while ( dirs.length ) {
-        var result = callback(dirs.join(path.sep));
+    var loc = path.resolve(file);
+    while (loc !== (loc = path.dirname(loc))) {
+        var result = callback(loc);
         if (typeof result !== 'undefined') return result;
-        dirs.pop();
     }
     return undefined;
 }

commit 34c6cf652d205fbe0bc9b24ad700f720ffa4459d

Restore eachParent behavior
The eachParent traversal modification in bc86e87e0e958e
caused eachParent to skip the first iteration, as it were,
compared to earlier behavior.

I.e. if you passed in `/foo/bar/quux`, 1.7.1 and before iterated
over `/foo/bar/quux`, `/foo/bar`, `/foo` and (erroneously) ``.

1.7.2 and later (with the modification in bc86e87e0e958e) iterated
over `/foo/bar`, `/foo` and (correctly) `/` given the same parameter.

This restores the 1.7.1 behavior, but with the `` bug corrected.

index.js
@@ -56,10 +56,10 @@ function eachParent(file, callback) {
     if ( file === false ) return undefined;
     if ( typeof file === 'undefined' ) file = '.';
     var loc = path.resolve(file);
-    while (loc !== (loc = path.dirname(loc))) {
+    do {
         var result = callback(loc);
         if (typeof result !== 'undefined') return result;
-    }
+    } while (loc !== (loc = path.dirname(loc)));
     return undefined;
 }
 