d2ad38e828c32cfabc50051ccc9710c8ea7b045e
Andrey Sitnik <andrey@sitnik.ru>
2018-01-30 22:54:47 +0300

    Improve defaults query tests

--- test/defaults.test.js
+++ test/defaults.test.js
@@ -4,3 +4,7 @@ it('selects defaults by keywords', () => {
   expect(browserslist('defaults, ie 6'))
     .toEqual(browserslist(browserslist.defaults.concat(['ie 6'])))
 })
+
+it('selects defaults case insensitive', () => {
+  expect(browserslist('Defaults')).toEqual(browserslist(browserslist.defaults))
+})
