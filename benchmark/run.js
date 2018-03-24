3aa4f4cc3869ad61462621dfaa4d73b8fae85177
Andrey Sitnik <andrey@sitnik.ru>
2018-03-24 02:43:33 -0400

    Fix benchmark JS code style

--- benchmark/run.js
+++ benchmark/run.js
@@ -1,35 +1,35 @@
 #!/usr/bin/env node
 
-var browserslist = require('..');
-var fs = require('fs');
-var path = require('path');
+var browserslist = require('..')
+var fs = require('fs')
+var path = require('path')
 
 var cases = [
-    {
-        file: path.join(__dirname, 'one/app/css/hello.css'),
-        n: 5000,
-        exists: true
-    },
-    {
-        file: path.join(__dirname, 'two/app/js/hello.js'),
-        n: 5000,
-        exists: true
-    },
-    {
-        file: path.join(__dirname, 'three/app/js/hello.js'),
-        n: 5000,
-        exists: false
-    }
-];
+  {
+    file: path.join(__dirname, 'one/app/css/hello.css'),
+    n: 5000,
+    exists: true
+  },
+  {
+    file: path.join(__dirname, 'two/app/js/hello.js'),
+    n: 5000,
+    exists: true
+  },
+  {
+    file: path.join(__dirname, 'three/app/js/hello.js'),
+    n: 5000,
+    exists: false
+  }
+]
 
 cases.forEach(function (test) {
-    if (!fs.existsSync(test.file)) {
-        throw new Error('invalid test');
+  if (!fs.existsSync(test.file)) {
+    throw new Error('invalid test')
+  }
+  for (var i = 0; i < test.n; i++) {
+    var result = browserslist.findConfig(test.file)
+    if (!!result !== test.exists) {
+      throw new Error('expected config to exist but it did not')
     }
-    for (var i = 0; i < test.n; i++) {
-        var result = browserslist.findConfig(test.file);
-        if (!!result !== test.exists) {
-            throw new Error('expected config to exist but it did not');
-        }
-    }
-});
+  }
+})
