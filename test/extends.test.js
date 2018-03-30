8b0a06830949a30be37813329b452b8203a7237a
Andrey Sitnik <andrey@sitnik.ru>
2018-03-30 13:32:48 -0400

    Less hacks for extends tests

--- test/extends.test.js
+++ test/extends.test.js
@@ -1,57 +1,78 @@
+var path = require('path')
+var fs = require('fs-extra')
+
 var browserslist = require('../')
 
-afterEach(() => {
-  jest.resetModules()
-})
+var mocked = []
 
-function mock (packageName, exports) {
-  jest.doMock(packageName, () => exports, { virtual: true })
+function mock (name, exports) {
+  var dir = path.join(__dirname, '..', 'node_modules', name)
+  mocked.push(dir)
+  return fs.ensureDir(dir).then(() => {
+    var content = 'module.exports = ' + JSON.stringify(exports)
+    return fs.writeFile(path.join(dir, 'index.js'), content)
+  })
 }
 
+afterEach(() => {
+  return Promise.all(mocked.map(dir => fs.remove(dir))).then(() => {
+    mocked = []
+  })
+})
+
 it('uses package', () => {
-  mock('browserslist-config-test', ['ie 11'])
-  var result = browserslist(['extends browserslist-config-test', 'edge 12'])
-  expect(result).toEqual(['edge 12', 'ie 11'])
+  return mock('browserslist-config-test', ['ie 11']).then(() => {
+    var result = browserslist(['extends browserslist-config-test', 'edge 12'])
+    expect(result).toEqual(['edge 12', 'ie 11'])
+  })
 })
 
 it('works with non-prefixed package with dangerousExtend', () => {
-  mock('pkg', ['ie 11'])
-  var result = browserslist(['extends pkg', 'edge 12'], {
-    dangerousExtend: true
+  return mock('pkg', ['ie 11']).then(() => {
+    var result = browserslist(['extends pkg', 'edge 12'], {
+      dangerousExtend: true
+    })
+    expect(result).toEqual(['edge 12', 'ie 11'])
   })
-  expect(result).toEqual(['edge 12', 'ie 11'])
 })
 
 it('handles scoped packages', () => {
-  mock('@scope/browserslist-config-test', ['ie 11'])
-  var result = browserslist(['extends @scope/browserslist-config-test'])
-  expect(result).toEqual(['ie 11'])
+  return mock('@scope/browserslist-config-test', ['ie 11']).then(() => {
+    var result = browserslist(['extends @scope/browserslist-config-test'])
+    expect(result).toEqual(['ie 11'])
+  })
 })
 
 it('handles scoped packages when package is @scope/browserslist-config', () => {
-  mock('@scope/browserslist-config', ['ie 11'])
-  var result = browserslist(['extends @scope/browserslist-config'])
-  expect(result).toEqual(['ie 11'])
+  return mock('@scope/browserslist-config', ['ie 11']).then(() => {
+    var result = browserslist(['extends @scope/browserslist-config'])
+    expect(result).toEqual(['ie 11'])
+  })
 })
 
 it('recursively imports configs', () => {
-  mock('browserslist-config-one', ['extends browserslist-config-two', 'ie 9'])
-  mock('browserslist-config-two', ['ie 10'])
-  var result = browserslist(['extends browserslist-config-one'])
-  expect(result).toEqual(['ie 10', 'ie 9'])
+  return Promise.all([
+    mock('browserslist-config-a', ['extends browserslist-config-b', 'ie 9']),
+    mock('browserslist-config-b', ['ie 10'])
+  ]).then(() => {
+    var result = browserslist(['extends browserslist-config-a'])
+    expect(result).toEqual(['ie 10', 'ie 9'])
+  })
 })
 
 it('handles relative queries in external packages with local overrides', () => {
-  mock('browserslist-config-test', ['ie 9-10'])
-  var result = browserslist(['extends browserslist-config-test', 'not ie 9'])
-  expect(result).toEqual(['ie 10'])
+  return mock('browserslist-config-rel', ['ie 9-10']).then(() => {
+    var result = browserslist(['extends browserslist-config-rel', 'not ie 9'])
+    expect(result).toEqual(['ie 10'])
+  })
 })
 
 it('throws when external package does not resolve to an array', () => {
-  mock('browserslist-config-test', { not: 'an array' })
-  expect(() => {
-    browserslist(['extends browserslist-config-test'])
-  }).toThrowError(/not an array/)
+  return mock('browserslist-config-wrong', { not: 'an array' }).then(() => {
+    expect(() => {
+      browserslist(['extends browserslist-config-wrong'])
+    }).toThrowError(/not an array/)
+  })
 })
 
 it('throws when package does not have browserslist-config- prefix', () => {
