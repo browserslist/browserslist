09b3d63aef9cf66a3bbc70986be98fee636942c0
Bryan Jones <bdkjones@VDK.local>
2018-09-12 21:49:43 -0700

    Fix CI Issues
    Sigh. Don’t copy/paste things.

--- cli.js
+++ cli.js
@@ -91,21 +91,20 @@ if (isArg('--help') || isArg('-h')) {
     browsers.forEach(function (browser) {
       process.stdout.write(browser + '\n')
     })
-    var stats
-    var worldResult = browserslist.coverage(browsers, stats)
+    var worldResult = browserslist.coverage(browsers, undefined)
     var roundWorld = Math.round(worldResult * 100) / 100.0
     process.stdout.write('coverage_global ' + roundWorld + '\n')
 
     var usResult = browserslist.coverage(browsers, 'us')
     var roundUS = Math.round(usResult * 100) / 100.0
     process.stdout.write('coverage_us ' + roundUS + '\n')
-  }
-  else if (mode === 'browsers') {
+
+  } else if (mode === 'browsers') {
     browsers.forEach(function (browser) {
       process.stdout.write(browser + '\n')
     })
-  } 
-  else {
+
+  } else {
     var stats
     if (country) {
       stats = country
