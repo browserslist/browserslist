bb6ef4ac622c2dc2bafd6bf1793ba00cbe96e803
Ivan Badichan <badican01117@gmail.com>
2020-02-19 15:52:07 +0100

    Update caniuse-lite with yarn (no test)

--- updateDB.js
+++ updateDB.js
@@ -4,8 +4,10 @@ var childProcess = require('child_process')
 var exec = childProcess.exec
 var execSync = childProcess.execSync
 
+var packageManager = 'npm'
+
 function updateDB () {
-  var lockfile = path.resolve(process.cwd(), 'package-lock.json')
+  var lockfile = getLockfile()
 
   console.log('Installing caniuse-lite ' + getLastVersion())
 
@@ -14,16 +16,14 @@ function updateDB () {
       throw readError
     }
 
-    var parsedData = JSON.parse(data)
-    delete parsedData.dependencies['caniuse-lite']
-    var newData = JSON.stringify(parsedData)
+    var newData = updateLockfile(data)
 
-    fs.writeFile('package-lock.json', newData, function (writeError) {
+    fs.writeFile(path.basename(lockfile), newData, function (writeError) {
       if (writeError) {
         throw writeError
       }
 
-      exec('npm install', function (installError) {
+      exec(packageManager + ' install', function (installError) {
         if (installError) {
           throw installError
         }
@@ -34,10 +34,42 @@ function updateDB () {
   })
 }
 
+function getLockfile () {
+  var packageLock = path.resolve(process.cwd(), 'package-lock.json')
+  var yarnLock = path.resolve(process.cwd(), 'yarn.lock')
+  var file = packageLock
+
+  if (fs.existsSync(yarnLock)) {
+    file = yarnLock
+    packageManager = 'yarn'
+  }
+
+  return file
+}
+
 function getLastVersion () {
   var output = execSync('npm show caniuse-lite version').toString()
   var lastVersion = output.split('\n')[0]
+
   return lastVersion
+}
+
+function updateLockfile (data) {
+  var parsedData, newData
+
+  try {
+    parsedData = JSON.parse(data)
+    delete parsedData.dependencies['caniuse-lite']
+    newData = JSON.stringify(parsedData)
+  } catch (error) {
+    parsedData = data.split('\n')
+    var filteredData = parsedData.filter(function (line) {
+      return !/^caniuse-lite/.test(line)
+    })
+    newData = filteredData.join('\n')
+  }
+
+  return newData
 }
 
 module.exports = updateDB
