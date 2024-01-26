179cb6afb030857f0f0ecf3250dcd1a154a93c5b
Marie-Eve <marie-eve.bourassa@radio-canada.ca>
2024-01-26 09:54:43 -0500

    Updated regex to have the option of adding an extension after @companyName but before /browserslist-config

--- node.js
+++ node.js
@@ -7,7 +7,7 @@ var BrowserslistError = require('./error')
 
 var IS_SECTION = /^\s*\[(.+)]\s*$/
 var CONFIG_PATTERN = /^browserslist-config-/
-var SCOPED_CONFIG__PATTERN = /@[^/]+\/browserslist-config(-|$|\/)/
+var SCOPED_CONFIG__PATTERN = /@[^/]+(?:\/[^/]+)?\/browserslist-config(?:-|$|\/)/
 var TIME_TO_UPDATE_CANIUSE = 6 * 30 * 24 * 60 * 60 * 1000
 var FORMAT =
   'Browserslist config should be a string or an array ' +
