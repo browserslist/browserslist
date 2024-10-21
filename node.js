b577b2dc47419e55350d5d479f6429f506e71b29
echo-vladimir <echo.vladimir.k@gmail.com>
2024-10-21 17:39:53 +0200

    Remove duplicate time

--- node.js
+++ node.js
@@ -416,13 +416,11 @@ module.exports = {
     if (process.env.BROWSERSLIST_IGNORE_OLD_DATA) return
 
     var latest = latestReleaseTime(agentsObj)
-    var currentTime = Date.now()
+    var monthsOld = Math.floor(
+      (Date.now() - latest) / (1000 * 60 * 60 * 24 * 30)
+    )
 
-    if (latest !== 0 && latest < currentTime - TIME_TO_UPDATE_CANIUSE) {
-      var monthsOld = Math.floor(
-        (currentTime - latest) / (1000 * 60 * 60 * 24 * 30)
-      )
-
+    if (latest !== 0 && monthsOld > 6) {
       console.warn(
         'Browserslist: caniuse-lite is ' +
           monthsOld +
