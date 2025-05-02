dcc4045b51c142318e2cc34c774af239ee0095c9
Andrey Sitnik <andrey@sitnik.ru>
2025-05-02 15:46:09 +0000

    Fix CI

--- test/extends.test.js
+++ test/extends.test.js
@@ -28,15 +28,17 @@ test('uses package', async () => {
   equal(result, ['ie 11', 'ie 6'])
 })
 
-test('uses ESM package', async () => {
-  let dir = join(__dirname, '..', 'node_modules', 'browserslist-config-esm')
-  mocked.push(dir)
-  await ensureDir(dir)
-  let content = 'export default ' + JSON.stringify(exports)
-  await writeFile(join(dir, 'index.js'), content)
-  let result = browserslist(['extends browserslist-config-esm', 'ie 6'])
-  equal(result, ['ie 6'])
-})
+if (parseInt(process.versions.node) >= 20) {
+  test('uses ESM package', async () => {
+    let dir = join(__dirname, '..', 'node_modules', 'browserslist-config-esm')
+    mocked.push(dir)
+    await ensureDir(dir)
+    let content = 'export default ' + JSON.stringify(exports)
+    await writeFile(join(dir, 'index.js'), content)
+    let result = browserslist(['extends browserslist-config-esm', 'ie 6'])
+    equal(result, ['ie 6'])
+  })
+}
 
 test('uses file in package', async () => {
   await mock('browserslist-config-test/ie', ['ie 11'])
